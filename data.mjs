6768316545b40bdb06b9294863d1860691803857
Alecto <fomenkoandrey1978@gmail.com>
2023-12-06 08:06:19 +0200

    res.end - шаблонная строка

---
+++ data.mjs
@@ -0,0 +1,20 @@
+const htmlTemplate = `
+    <!DOCTYPE html>
+    <html lang='en'>
+    
+    <head>
+      <meta charset='UTF-8'>
+      <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+      <title>HTTP server</title>
+    </head>
+    
+    <body style='font-family: Arial, sans-serif'>
+      <div style='width: min(100% - 40px, 992px); margin-inline: auto;'>
+          <h1>Hello from HTTP server</h1>
+      </div>  
+    </body>
+    
+    </html>
+    `
+
+export { htmlTemplate }
